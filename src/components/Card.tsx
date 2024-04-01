bb32f5bb0fb5716bf50f5bd8de7d2b3314343d15
Sahnwaz786 <mdsahnwazalam48@gmail.com>
2024-04-01 21:35:50 +0530

    image updated

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -28,7 +28,7 @@ const MediaCard: React.FC = () => {
           >
             <CardMedia
               sx={{ height: 400 }}
-              image="https://s3-alpha-sig.figma.com/img/ceed/8067/dc33760174d3f67737f6e318d0f118ff?Expires=1711929600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=gRi10t64DHbb7CKAQ32uZ8r5GgEveA16AhaoyJEq5xxP-SI1b7CiBLkszXQXdS713LL7XKUWF8lKLmJUBIHFgNwgoxU7Q9nUnTlunB5FYP~lcjUv94Cz3te24QyiQnWAXIYz8fW7GX3EBT4Fm9fCAf3N4UG9T9VBZmPdj48aJydkpc4ghL1a~Vxl2ffVb60sIf7aGozJWUMLb19ldALA1PIcnDhX4fLianO0lSzhCaZqBocge45saoIJsWh-Namu5euhcgWNzj4jjaLFrh40McHoPYSz2FJkcgSqcfGTQhpa9-5YidZ0H6j-Uo2C~uB2DgFW5uPwJWNq~9Ow~j12qQ__"
+              image="../public/images/red shirt.jpeg"
               title="green iguana"
             />
            
@@ -77,7 +77,7 @@ const MediaCard: React.FC = () => {
           >
             <CardMedia
               sx={{ height: 400 }}
-              image="https://s3-alpha-sig.figma.com/img/a24a/d1e0/76e2b366b2456bbd169b0c3c9525252c?Expires=1711929600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Nkp1Is~dcRKVSVj8GNhxNLMJ7gTEzNDkf8VtD-OryJ0sykh1lwmV-ewiigHPS4njhKjcfYNYHzNtka9sa6DQdjxPUZR6L9ziTFWXse30L5RapCCt5H7T5jsZsMoP09PSNj4-HiVCjoqXwVAVEAhZm~f924bZogIfzfxFc0gC3OPjXnfM2b3~W2jRmsFQJYQxHVAHE2c0YUjzBlbFgDwCD5VY6C~tF71RtERjrsoOZMrojXSQzoPhqcCyNZY5lTX7IFlGbpi-1d4QbKSqih3hrizl6afTuUEfMU-y2fpz3lLyQ3EWMCMLQ8RwiBLAvOcsMkDiPhzFhKCJR0~L6GgGBQ__"
+              image="../public/images/blue shirt.jpeg"
               title="green iguana"
             />
             <CardContent>
@@ -124,7 +124,7 @@ const MediaCard: React.FC = () => {
           >
             <CardMedia
               sx={{ height: 400 }}
-              image="https://s3-alpha-sig.figma.com/img/b290/2f7a/b2fcf21c696811f745109da24a740d73?Expires=1711929600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=XtF-TuNcma0-HNbqtENI51868JjNd38EzT5jARpBJ7hvmYBWokB-dZmajh5EMHlo2vJzQl9gymCpvrM7j-OLsXKViFlh7n7qb~M8onQwwDoSvBvPrbH~d1~5N3rn6XxrGb742E~ZWxs83gLMTv8QsQ202LGEzCght4GGoLiPW7Y3aC4ADsJjQGWCoFD-TiRG8PQxSAnlgncjQAWThvQLxl695HYHxq4VShwiSl5CBfH-20XVdVi0W6gEt71vHHuQTSgOcG5eir2lvYRIvY3qLXixYYJZvT72XXJklWzu19p515wWV0QI5LTKLnfRvpPI1s9D5dRrk4tAFZGTC2J3fw__"
+              image="../public/images/helmate boy.jpeg"
               title="green iguana"
             />
             <CardContent>
@@ -171,7 +171,7 @@ const MediaCard: React.FC = () => {
           >
             <CardMedia
               sx={{ height: 400 }}
-              image="https://s3-alpha-sig.figma.com/img/a24a/d1e0/76e2b366b2456bbd169b0c3c9525252c?Expires=1711929600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Nkp1Is~dcRKVSVj8GNhxNLMJ7gTEzNDkf8VtD-OryJ0sykh1lwmV-ewiigHPS4njhKjcfYNYHzNtka9sa6DQdjxPUZR6L9ziTFWXse30L5RapCCt5H7T5jsZsMoP09PSNj4-HiVCjoqXwVAVEAhZm~f924bZogIfzfxFc0gC3OPjXnfM2b3~W2jRmsFQJYQxHVAHE2c0YUjzBlbFgDwCD5VY6C~tF71RtERjrsoOZMrojXSQzoPhqcCyNZY5lTX7IFlGbpi-1d4QbKSqih3hrizl6afTuUEfMU-y2fpz3lLyQ3EWMCMLQ8RwiBLAvOcsMkDiPhzFhKCJR0~L6GgGBQ__"
+              image="../public/images/blue shirt.jpeg"
               title="green iguana"
             />
             <CardContent>
@@ -218,7 +218,7 @@ const MediaCard: React.FC = () => {
           >
             <CardMedia
               sx={{ height: 263 }}
-              image="https://wallpaperaccess.com/full/5255765.jpg"
+              image="../public/images/Dicks.jpeg"
               title="green iguana"
             />
             <CardContent>
--- src/components/Collections.tsx
+++ src/components/Collections.tsx
@@ -48,7 +48,7 @@ const Collections: React.FC = () => {
           >
             <CardMedia
               sx={{ height: 400 }}
-              image="https://s3-alpha-sig.figma.com/img/bc7b/d2d1/a2c7e5def5b347470305864869a894b6?Expires=1711929600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=BZDcos--1UzSQRlT-N9cx65GfMLCdY1bDgUpPVGWaSaeNFqn5f3mWqPPavOC3dwIVAQ5LSFSV87f4nqFWTL47SpM9SAoK-Hik~QDb9r9ZcMQi0f2bLc~CwVSIVqTWUUErwccA~nln3KASNGTwveym6PUgIPSqPuS0TzUtzpGFgI80gg31qkC0KXNRC--XF6vFtVP5szniy~A5-hhak8Ct-WgvgQLqVThJm~LXpDkB0NIVZ8NQ9O2y6whQDm0gf7Ex7i03oFtipY19gbTtPAr6mzV-0RZKivl-0woyvGsK-aA1JzKvWYgzExzhjqGFCca1yvb2KbomyjjpZHxtvZV-A__"
+              image="../public/images/w.jpeg"
               title="green iguana"
             />
             <div className="text-zinc-400 ... mt-5 ml-1">
@@ -104,7 +104,7 @@ const Collections: React.FC = () => {
           >
             <CardMedia
               sx={{ height: 400 }}
-              image="https://s3-alpha-sig.figma.com/img/3de4/4e83/b6e3aeda879dc6f22f94711c788789b5?Expires=1711929600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=nUF~1EVyrZGHV5Y2m287PwrLOJlkr894fLVacxtljXvPD4wxG9g4D2HNAj17sY960ZApdFZ7vZXHvZjq0~7Vb0wM3nF3b6Vi4SHLk4ztO3tAouszbmpZp~d~lEZl3-95hpbTXgrwtNGB8ujsBxQL5KUsZwG3wCPXLt61kJIpz2pmCQcmY9vS-JuNgfHT13F0ObY4jJSYeFrRcihRJfTmLxzOCbyXqncFRQhnBCuL50D-E1XAHNLFOX5AXJVnBoisGHVAYJbjfoz3UWH0g-bApbnLTUyR5vEZUBdIwoQISiaSttjGTQD6NhpHyKXKHWuE3yVUN2jmWzVwOnvPzn1nEA__"
+              image="../public/images/RC.jpeg"
               title="green iguana"
             />
             <div className="text-zinc-400 ... mt-5 ml-1">
@@ -160,7 +160,7 @@ const Collections: React.FC = () => {
           >
             <CardMedia
               sx={{ height: 400 }}
-              image="https://s3-alpha-sig.figma.com/img/bc7b/d2d1/a2c7e5def5b347470305864869a894b6?Expires=1711929600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=BZDcos--1UzSQRlT-N9cx65GfMLCdY1bDgUpPVGWaSaeNFqn5f3mWqPPavOC3dwIVAQ5LSFSV87f4nqFWTL47SpM9SAoK-Hik~QDb9r9ZcMQi0f2bLc~CwVSIVqTWUUErwccA~nln3KASNGTwveym6PUgIPSqPuS0TzUtzpGFgI80gg31qkC0KXNRC--XF6vFtVP5szniy~A5-hhak8Ct-WgvgQLqVThJm~LXpDkB0NIVZ8NQ9O2y6whQDm0gf7Ex7i03oFtipY19gbTtPAr6mzV-0RZKivl-0woyvGsK-aA1JzKvWYgzExzhjqGFCca1yvb2KbomyjjpZHxtvZV-A__"
+              image="../public/images/w.jpeg"
               title="green iguana"
             />
             <div className="text-zinc-400 ... mt-5 ml-1">
